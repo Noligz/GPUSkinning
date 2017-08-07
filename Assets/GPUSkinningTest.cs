cafd593496a3c41a4bd2d05ba39060c8f7bec566
Noligz <noligz@163.com>
2017-08-07 15:03:15 +0800

    init commit

---
+++ Assets/GPUSkinningTest.cs
@@ -0,0 +1,187 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class GPUSkinningTest : MonoBehaviour
+{
+    public AnimationClip clip;
+    public SkinnedMeshRenderer smr;
+    public float targetFPS = 30f;
+
+    [ContextMenu("CreateMatrixTexture")]
+    public void CreateMatrixTexture()
+    {
+        var deltaTime = 1f / targetFPS;
+        int curPixelIdx = 0;
+        var boneCnt = smr.bones.Length;
+        var matrixs = new Matrix4x4[boneCnt];
+        int frameCnt = (int)(clip.length * targetFPS);
+        int textureWidth = 1, textureHeight = 1;
+        var pixelsPerMatrix = 3;
+        CalMatrixTextureSize(boneCnt * frameCnt * pixelsPerMatrix, out textureWidth, out textureHeight);
+        var texture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBAHalf, false, true);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        var pixels = texture.GetPixels();
+        for (float curTime = 0; curTime < clip.length; curTime += deltaTime)
+        {
+            for (int i = 0; i < boneCnt; i++)
+            {
+                matrixs[i] = GetBoneMatrixFromClip(clip, curTime, smr, i);
+            }
+
+            RecursiveMultiplyMatrix(smr.rootBone, smr, Matrix4x4.identity, matrixs);
+
+            for (int i = 0; i < boneCnt; i++)
+            {
+                var m = matrixs[i];
+                pixels[curPixelIdx++] = new Color(m.m00, m.m01, m.m02, m.m03);
+                pixels[curPixelIdx++] = new Color(m.m10, m.m11, m.m12, m.m13);
+                pixels[curPixelIdx++] = new Color(m.m20, m.m21, m.m22, m.m23);
+            }
+        }
+        texture.SetPixels(pixels);
+        texture.Apply();
+
+        AssetDatabase.CreateAsset(texture, "Assets/Generated/AnimTexture.asset");
+
+        AssetDatabase.Refresh();
+
+        Debug.LogErrorFormat("PixelPerFrame = {0}, FrameCount = {1}", boneCnt * pixelsPerMatrix, frameCnt);
+    }
+
+    string GetBonePath(Transform bone, Transform root)
+    {
+        string ret = bone.name;
+        var tran = bone;
+        while(tran != root && tran.parent != null)
+        {
+            tran = tran.parent;
+            ret = string.Format("{0}/{1}", tran.name, ret);
+        }
+        return ret;
+    }
+
+    int GetBoneIdx(Transform bone, Transform[] bones)
+    {
+        for (int i = 0, length = bones.Length; i < length; i++)
+        {
+            if (bone == bones[i])
+                return i;
+        }
+        return -1;
+    }
+
+    Vector2 CalMatrixTextureSize(int pixelCnt, out int width, out int height)
+    {
+        width = 1;
+        height = 1;
+        var flag = true;
+        while(width * height < pixelCnt)
+        {
+            if (flag)
+                width *= 2;
+            else
+                height *= 2;
+            flag = !flag;
+        }
+        return new Vector2(width, height);
+    }
+
+    Matrix4x4 GetBoneMatrixFromClip(AnimationClip clip, float time, SkinnedMeshRenderer smr, int boneIdx)
+    {
+        var b = smr.bones[boneIdx];
+        var path = GetBonePath(b, smr.rootBone);
+        var curveBinding = new EditorCurveBinding() { path = path, type = typeof(Transform) };
+
+        curveBinding.propertyName = "m_LocalPosition.x";
+        var posX = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalPosition.y";
+        var posY = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalPosition.z";
+        var posZ = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        var pos = new Vector3(posX, posY, posZ);
+
+        curveBinding.propertyName = "m_LocalRotation.x";
+        var rotX = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalRotation.y";
+        var rotY = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalRotation.z";
+        var rotZ = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalRotation.w";
+        var rotW = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        var tmpRot = new Vector4(rotX, rotY, rotZ, rotW);
+        tmpRot = tmpRot.normalized;
+        var rot = new Quaternion(tmpRot.x, tmpRot.y, tmpRot.z, tmpRot.w);
+
+        curveBinding.propertyName = "m_LocalScale.x";
+        var scaleX = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalScale.y";
+        var scaleY = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        curveBinding.propertyName = "m_LocalScale.z";
+        var scaleZ = AnimationUtility.GetEditorCurve(clip, curveBinding).Evaluate(time);
+        var scale = new Vector3(scaleX, scaleY, scaleZ);
+
+        return Matrix4x4.TRS(pos, rot, scale);
+    }
+
+    void RecursiveMultiplyMatrix(Transform bone, SkinnedMeshRenderer smr, Matrix4x4 parentMatrix, Matrix4x4[] matrixs)
+    {
+        var bones = smr.bones;
+        var boneIdx = GetBoneIdx(bone, bones);
+        var m = parentMatrix * matrixs[boneIdx];
+        for (int i = 0, length = bone.childCount; i < length; i++)
+        {
+            var child = bone.GetChild(i);
+            RecursiveMultiplyMatrix(child, smr, m, matrixs);
+        }
+        matrixs[boneIdx] = m * smr.sharedMesh.bindposes[boneIdx];
+    }
+
+    private class BoneWeightPair : System.IComparable<BoneWeightPair>
+    {
+        public int index = 0;
+        public float weight = 0;
+
+        public int CompareTo(BoneWeightPair b)
+        {
+            return weight > b.weight ? -1 : 1;
+        }
+    }
+
+    [ContextMenu("CreateMesh")]
+    void CreateMesh()
+    {
+        var originMesh = smr.sharedMesh;
+        var generateMesh = new Mesh();
+        generateMesh.vertices = originMesh.vertices;
+        generateMesh.uv = originMesh.uv;
+        generateMesh.tangents = originMesh.tangents;
+        generateMesh.triangles = originMesh.triangles;
+        generateMesh.bounds = originMesh.bounds;
+        generateMesh.colors = originMesh.colors;
+        generateMesh.normals = originMesh.normals;
+
+        List<Vector4> boneIdxWight01 = new List<Vector4>(originMesh.vertexCount);
+        List<Vector4> boneIdxWight23 = new List<Vector4>(originMesh.vertexCount);
+        for (int i = 0, length = originMesh.vertexCount; i < length; i++)
+        {
+            var boneWeight = originMesh.boneWeights[i];
+            BoneWeightPair[] boneWeightPairs = new BoneWeightPair[4];
+            boneWeightPairs[0] = new BoneWeightPair() { index = boneWeight.boneIndex0, weight = boneWeight.weight0 };
+            boneWeightPairs[1] = new BoneWeightPair() { index = boneWeight.boneIndex1, weight = boneWeight.weight1 };
+            boneWeightPairs[2] = new BoneWeightPair() { index = boneWeight.boneIndex2, weight = boneWeight.weight2 };
+            boneWeightPairs[3] = new BoneWeightPair() { index = boneWeight.boneIndex3, weight = boneWeight.weight3 };
+            System.Array.Sort(boneWeightPairs);
+
+            boneIdxWight01.Add(new Vector4(boneWeightPairs[0].index, boneWeightPairs[0].weight, boneWeightPairs[1].index, boneWeightPairs[1].weight));
+            boneIdxWight23.Add(new Vector4(boneWeightPairs[2].index, boneWeightPairs[2].weight, boneWeightPairs[3].index, boneWeightPairs[3].weight));
+        }
+        generateMesh.SetUVs(1, boneIdxWight01);
+        generateMesh.SetUVs(2, boneIdxWight23);
+
+        AssetDatabase.CreateAsset(generateMesh, "Assets/Generated/Mesh.asset");
+    }
+
+}
---
+++ Assets/GPUSkinningTest.shader
@@ -0,0 +1,106 @@
+﻿Shader "Unlit/GPUSkinningTest"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_AnimTex ("Anim", 2D) = "white" {}
+		_AnimTexWidth ("AnimTexWidth", float) = 1
+		_AnimTexHeight ("AnimTexHeight", float) = 1
+		_AnimFrameCount ("AnimFrameCount", float) = 1
+		_PixelPerFrame ("PixelPerFrame", float) = 1
+		_FPS ("FPS", float) = 30
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float4 uv1 : TEXCOORD1;
+				float4 uv2 : TEXCOORD2;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _AnimTex;
+			float _AnimTexWidth, _AnimTexHeight;
+			float _PixelPerFrame;
+			float _FPS;
+			float _AnimFrameCount;
+
+			float4 IdxToUV(uint idx)
+			{
+				float4 ret;
+				uint texWidth = (uint)_AnimTexWidth;
+				uint texHeight = (uint)_AnimTexHeight;
+				uint row = idx / texWidth;
+				uint column = idx - row * texWidth;
+				ret.x = (float)column / texWidth;
+				ret.y = (float)row / texHeight;
+				ret.z = 0;
+				ret.w = 0;
+				return ret;
+			}
+
+			float4x4 GetMatrix(uint frameStartIdx, uint boneIdx)
+			{
+				uint idx = frameStartIdx + boneIdx * 3;
+				float4 pos = tex2Dlod(_AnimTex, IdxToUV(idx));
+				float4 rot = tex2Dlod(_AnimTex, IdxToUV(idx+1));
+				float4 sca = tex2Dlod(_AnimTex, IdxToUV(idx+2));
+				return float4x4(pos, rot, sca, float4(0,0,0,1));
+			}
+			
+			v2f vert (appdata v)
+			{
+				uint frameIdx = ((uint)(_Time.y * _FPS)) % ((uint)(_AnimFrameCount));
+				uint frameStartIdx = frameIdx * _PixelPerFrame;
+
+				uint bone0Idx = (uint)v.uv1.x;
+				float bone0Weight = 1;//v.uv1.y;
+				float4 pos = mul(GetMatrix(frameStartIdx, bone0Idx), v.vertex) * bone0Weight;
+
+				// int bone1Idx = (int)v.uv1.z;
+				// float bone1Weight = v.uv1.w;
+				// pos += mul(GetMatrix(frameStartIdx, bone1Idx), v.vertex) * bone1Weight;
+
+				// int bone2Idx = (int)v.uv2.x;
+				// float bone2Weight = v.uv2.y;
+				// pos += mul(GetMatrix(frameStartIdx, bone2Idx), v.vertex) * bone2Weight;
+
+				// int bone3Idx = (int)v.uv2.z;
+				// float bone3Weight = v.uv2.w;
+				// pos += mul(GetMatrix(frameStartIdx, bone3Idx), v.vertex) * bone3Weight;
+
+				v2f o;
+				o.vertex = UnityObjectToClipPos(pos);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
